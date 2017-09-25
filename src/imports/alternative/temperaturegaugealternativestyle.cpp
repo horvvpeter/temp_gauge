33bb78cde443c71004fac70d5c7a81d2caf2ff33
Peter Horvath <horvvpeter@gmail.com>
2017-09-25 17:41:04 +0200

    Create alternative style

--- src/imports/alternative/temperaturegaugealternativestyle.cpp
+++ src/imports/alternative/temperaturegaugealternativestyle.cpp
@@ -7,9 +7,39 @@ TemperatureGaugeAlternativeStyle::TemperatureGaugeAlternativeStyle(QObject *pare
 {
 }
 
-QColor TemperatureGaugeAlternativeStyle::backgroundColor() const
+QColor TemperatureGaugeAlternativeStyle::backgroundColorStart() const
 {
-    return QColor::fromRgba(0xFF00FFFF);
+    return QColor::fromRgb(0x364254);
+}
+
+QColor TemperatureGaugeAlternativeStyle::backgroundColorEnd() const
+{
+    return QColor::fromRgb(0x1f2226);
+}
+
+QColor TemperatureGaugeAlternativeStyle::positiveColorStart() const
+{
+    return QColor::fromRgb(0xa22c1e);
+}
+
+QColor TemperatureGaugeAlternativeStyle::positiveColorEnd() const
+{
+    return QColor::fromRgb(0xff230a);
+}
+
+QColor TemperatureGaugeAlternativeStyle::negativeColorStart() const
+{
+    return QColor::fromRgb(0x0f454f);
+}
+
+QColor TemperatureGaugeAlternativeStyle::negativeColorEnd() const
+{
+    return QColor::fromRgb(0x1f8ea3);
+}
+
+QColor TemperatureGaugeAlternativeStyle::tickColor() const
+{
+    return QColor::fromRgb(0x262424);
 }
 
 QT_END_NAMESPACE

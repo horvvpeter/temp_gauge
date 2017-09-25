1d519196afb4850933ee8267cbeb61337fe5d193
Peter Horvath <horvvpeter@gmail.com>
2017-09-25 16:59:00 +0200

    Create simple default style

--- src/imports/default/temperaturegaugedefaultstyle.cpp
+++ src/imports/default/temperaturegaugedefaultstyle.cpp
@@ -9,7 +9,17 @@ TemperatureGaugeDefaultStyle::TemperatureGaugeDefaultStyle(QObject *parent) :
 
 QColor TemperatureGaugeDefaultStyle::backgroundColor() const
 {
-    return QColor::fromRgba(0xFF0000FF);
+    return QColor::fromRgba(0xFF727984);
+}
+
+QColor TemperatureGaugeDefaultStyle::valueColor() const
+{
+    return QColor::fromRgba(0xFFFF0000);
+}
+
+QColor TemperatureGaugeDefaultStyle::tickColor() const
+{
+    return QColor::fromRgba(0xFF000000);
 }
 
 QT_END_NAMESPACE
